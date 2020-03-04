8e783d70333076ffd35af9dac5db3457d3ff3f5f
Peter <pvegh@posteo.uk>
2020-03-04 17:59:31 +0000

    Packaging

---
+++ genealloy/__init__.py
@@ -0,0 +1,3 @@
+from .codontable import (codon_to_aa, aa_to_codon_extended, generate_swaptable)
+from .genealloy import (convert_seq_to_codons, convert_codonlist_to_tuplelist, find_matches, report_results)
+from .version import __version__
