628ea99c409ac5860ac391601b9f735b33ee6ecd
Peter <pvegh@posteo.uk>
2020-03-05 13:46:12 +0000

    Black formatting

--- genealloy/__init__.py
+++ genealloy/__init__.py
@@ -1,3 +1,15 @@
-from .codontable import (codon_to_aa, aa_to_codon_extended, codon_extended_to_aa, generate_swaptable)
-from .genealloy import (convert_seq_to_codons, convert_codonlist_to_tuplelist, find_matches, report_results, calculate_aa_solutions, calculate_levenshtein_distances)
+from .codontable import (
+    codon_to_aa,
+    aa_to_codon_extended,
+    codon_extended_to_aa,
+    generate_swaptable,
+)
+from .genealloy import (
+    convert_seq_to_codons,
+    convert_codonlist_to_tuplelist,
+    find_matches,
+    report_results,
+    calculate_aa_solutions,
+    calculate_levenshtein_distances,
+)
 from .version import __version__
--- genealloy/genealloy.py
+++ genealloy/genealloy.py
@@ -5,7 +5,8 @@ except ImportError:
 else:
     has_pkgs = True
 
-from .codontable import (codon_to_aa, aa_to_codon_extended, codon_extended_to_aa)
+from .codontable import codon_to_aa, aa_to_codon_extended, codon_extended_to_aa
+
 
 def convert_seq_to_codons(seq):
     seq_codons = [seq[i : i + 3] for i in range(0, len(seq), 3)]
@@ -31,8 +32,10 @@ def find_matches(parasite_tuplelist, host_tuplelist):
         parasite_index = 0
         has_match = True
         for parasite_index in range(0, len_parasite):
-            common_codons = set(parasite_tuplelist[parasite_index]) & set(host_tuplelist[begin_index + parasite_index])
-            if  common_codons == set():
+            common_codons = set(parasite_tuplelist[parasite_index]) & set(
+                host_tuplelist[begin_index + parasite_index]
+            )
+            if common_codons == set():
                 has_match = False
                 break
             else:
@@ -46,26 +49,30 @@ def find_matches(parasite_tuplelist, host_tuplelist):
 
 def report_results(index_of_matches):
     if index_of_matches == dict():
-        print("The guest (parasite) sequence cannot be inserted into the host sequence.")
+        print(
+            "The guest (parasite) sequence cannot be inserted into the host sequence."
+        )
         return
-    
+
     print("The number of matching substrings in host is: %d," % len(index_of_matches))
     positions = list(index_of_matches.keys())
     print("at host codon position(s): %s" % positions)
 
 
 def calculate_aa_solutions(seq_codons, index_of_matches):
-    parasite_aa_seq = ''.join(codon_to_aa[c] for c in seq_codons)
+    parasite_aa_seq = "".join(codon_to_aa[c] for c in seq_codons)
     solutions = dict()
-    solutions['parasite'] = parasite_aa_seq
+    solutions["parasite"] = parasite_aa_seq
     for key, match in index_of_matches.items():
-        solutions[key] = ''
+        solutions[key] = ""
         for index, codon in enumerate(seq_codons):
             aa = codon_to_aa[codon]
             if set(aa_to_codon_extended[aa]) & set(match[index]) != set():
                 solutions[key] += aa
             else:
-                solutions[key] += codon_extended_to_aa[match[index][0]]  # take the 1st aa
+                solutions[key] += codon_extended_to_aa[
+                    match[index][0]
+                ]  # take the 1st aa
 
     return solutions
 
@@ -74,8 +81,8 @@ def calculate_levenshtein_distances(aa_solutions):
     if not has_pkgs:
         raise ImportError("Function requires the python-Levenshtein package.")
 
-    parasite = aa_solutions['parasite']
-    del aa_solutions['parasite']
+    parasite = aa_solutions["parasite"]
+    del aa_solutions["parasite"]
 
     levenshtein_distances = dict()
     for key, aa_seq in aa_solutions.items():
